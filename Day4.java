481608dfc95667cc2ddd806b734a1b8ea80a518e
kyleY9 <146113920+kyleY9@users.noreply.github.com>
2024-12-11 13:12:05 -0500

    Finished Day 4 Part 1!

--- Day4.java
+++ Day4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Day4 {
     public static void main(String[] args) {
-        ArrayList<String> fileData = getFileData("src/Sample.txt");
+        ArrayList<String> fileData = getFileData("src/Day4Input.txt");
 
         int rows = fileData.size();
         int columns = fileData.get(0).length();
@@ -16,20 +16,63 @@ public class Day4 {
                 grid[r][c] = fileData.get(r).substring(c, c+1);
             }
         }
-
-        System.out.println(diagonal(grid));
+        System.out.println("Num Hor: " + horizontal(grid));
+        System.out.println("Num Ver: " + vertical(grid));
+        System.out.println("Num Diagonals: " + diagonal(grid));
+        System.out.println("Total Matches: " + (horizontal(grid) + vertical(grid) + diagonal(grid)));
 
         // "grid" represents a 2D array of Strings built from the input file
 
     }
 
+    public static int horizontal(String[][] grid) {
+        int matches = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c].equals("X")) {
+                    if (c + 3 < grid[0].length && grid[r][c + 1].equals("M")) { // horizontal right
+                        if (grid[r][c + 2].equals("A")) {
+                            if (grid[r][c + 3].equals("S")) {
+                                matches++;
+                            }
+                        }
+                    }
+                    if (c - 3 >= 0 && grid[r][c - 1].equals("M")) { // horizontal left
+                        if (grid[r][c - 2].equals("A")) {
+                            if (grid[r][c - 3].equals("S")) {
+                                matches++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return matches;
+    }
+
     public static int vertical(String[][] grid) {
         int matches = 0;
         for (int r = 0; r < grid.length; r++) {
-            for (int c= 0; c < grid[0].length; c++) {
-
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c].equals("X")) {
+                    if (r + 3 < grid.length && grid[r + 1][c].equals("M")) { // vertical down
+                        if (grid[r + 2][c].equals("A")) {
+                            if (grid[r + 3][c].equals("S")) {
+                                matches++;
+                            }
+                        }
+                    }
+                    if (r - 3 >= 0 && grid[r - 1][c].equals("M")) { // vertical up
+                        if (grid[r - 2][c].equals("A")) {
+                            if (grid[r - 3][c].equals("S")) {
+                                matches++;
+                            }
+                        }
+                    }
+                }
             }
         }
+        return matches;
     }
 
     public static int diagonal(String[][] grid) {
