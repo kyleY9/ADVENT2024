4941ecf85758b7c771b7ab34362da2509a720fe5
kyleY9 <146113920+kyleY9@users.noreply.github.com>
2024-12-17 13:21:35 -0500

    Almost done!

--- Day5.java
+++ Day5.java
@@ -1,12 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Day5 {
     public static void main(String[] args) {
-        ArrayList<String> fileData = getFileData("src/Day5Input.txt");
-        System.out.println(fileData);
+        ArrayList<String> fileData = getFileData("src/Sample.txt");
         int total = 0;
         ArrayList<String> rules = new ArrayList<>();
         ArrayList<String> lists = new ArrayList<>();
@@ -18,32 +18,33 @@ public class Day5 {
             }
         }
 
-        for (int i = 0; i < lists.size(); i++) {
+        System.out.println(rules);
+        System.out.println(lists);
+
+        for (String list : lists) {
             ArrayList<Integer> myList = new ArrayList<>();
-            for (String rule: rules) {
-                String numOne = rule.split("|")[0];
-                String numTwo = rule.split("|")[1];
-                if (lists.get(i).contains(numOne) && lists.get(i).contains(numTwo)) {
+            for (String rule : rules) {
+                String numOne = rule.split("\\|")[0];
+                String numTwo = rule.split("\\|")[1];
+                if (list.contains(numOne) && list.contains(numTwo)) {
                     myList = new ArrayList<>();
-                     String[] temp = lists.get(i).split(",");
-                     for (String str : temp) {
-                         myList.add(Integer.parseInt(str));
-                     }
+                    String[] temp = list.split(",");
+                    for (String str : temp) {
+                        myList.add(Integer.parseInt(str));
+                    }
 
-                     int index1 = myList.indexOf(Integer.parseInt(numOne));
-                     int index2 = myList.indexOf(Integer.parseInt(numTwo));
+                    int index1 = myList.indexOf(Integer.parseInt(numOne));
+                    int index2 = myList.indexOf(Integer.parseInt(numTwo));
 
-                     if (index2 < index1) {
-                         myList.remove(index2);
-                         myList.add(index2, Integer.parseInt(numOne));
-                         myList.remove(index1);
-                         myList.add(index1, Integer.parseInt(numTwo));
-                     }
+                    if (index1 < index2) {
+                        total++;
+                    }
+
                 }
             }
-            System.out.println(myList);
-            System.out.println(myList.get(myList.size() / 2));
-            total += myList.get(myList.size() / 2);
+//            System.out.println(myList);
+//            System.out.println(myList.get(myList.size() / 2));
+//            total += myList.get(myList.size() / 2);
         }
         System.out.println(total);
     }
